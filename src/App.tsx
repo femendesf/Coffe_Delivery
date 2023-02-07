27055586f872fa1cab44395f09769278c295716e
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2023-02-07 14:27:34 -0300

    Project created
    Arquivos do projeto adicionado

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.tsx'
+
+export function App() {
+
+  return (
+      <div>
+          <h1 className='text-black'>
+            Hello World
+          </h1>
+          <img src="src\assets\expresso_tradicional.svg" alt="" />
+      </div>
+     
+  )
+}
+
+
